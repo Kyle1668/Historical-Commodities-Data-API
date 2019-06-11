836e18c333d723edca98aa704d86324d7b4aff0a
kyle1668 <kyledevinobrien1@gmail.com>
2019-06-11 15:59:24 -0700

    wrote api handler for incorrect or missing commodity type

--- api/commodities_endpoints.py
+++ api/commodities_endpoints.py
@@ -1,7 +1,7 @@
 import numpy as np
 from psycopg2 import Error
 from flask import jsonify
-from flask_restful import Resource, reqparse
+from flask_restful import Resource
 from database import postgres_connection
 from flask import request, make_response
 
@@ -9,10 +9,10 @@ from flask import request, make_response
 class CommoditiesEndpoints(Resource):
 
     def get(self):
-        print(request.args)
-
         if request.args.get("start_date") is None or request.args.get("end_date") is None:
             return self.missing_query_param_response()
+        elif request.args.get("commodity_type") not in ["gold", "silver"]:
+            return self.incorrect_commodity_type_response(request.args.get("commodity_type"))
 
         data = self.get_time_series_data(
             postgres_connection,
@@ -45,7 +45,13 @@ class CommoditiesEndpoints(Resource):
         return make_response(json, 400)
 
     def incorrect_commodity_type_response(self, wrong_commodity):
-        pass
+        missing_message = "Missing query parameter commodity (gold/silver)"
+        incorrect_message = "Incorrect commodity type (not gold or silver)"
+        json = jsonify({
+            "error": True,
+            "message": incorrect_message if wrong_commodity else missing_message
+        })
+        return make_response(json, 400)
 
     def get_time_series_data(self, pg_db_connection, start_date, end_date, commodity):
         cursor = pg_db_connection.cursor()
